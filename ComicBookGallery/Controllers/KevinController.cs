8e9cffec9fc04b46e87d92d56635d105d69c333e
Kevin <KRyan92@outlook.comn>
2018-05-13 02:30:25 -0400

    Created Comic Books Controller

---
+++ ComicBookGallery/Controllers/KevinController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+	public class ComicBooksController : Controller
+    {
+        public string Details()
+		{
+			return "Hello From the Comic Books Controller";
+
+        }
+    }
+}
