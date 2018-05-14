c03d82666cf88cf76e3e2fa89e079ea8aab9ff2a
Kevin <KRyan92@outlook.comn>
2018-05-14 00:20:12 -0400

    updates model

---
+++ ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+       
+			public string Actor { get; set; }
+			public string Role { get; set; }
+        
+    }
+}
